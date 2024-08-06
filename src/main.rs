84df2a1c5c8ec8864407f8817234b0f74ec9879f
Georgij Seidler <gseid55@gmail.com>
2024-08-06 20:32:53 +0200

    added tests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use lazy_static::lazy_static;
 use regex::Regex;
 use std::io;
+
+lazy_static! {
+    static ref RE_NUMBER: Regex = Regex::new(r"\d+(\.\d+)?").unwrap();
+    static ref RE_OPERATOR: Regex = Regex::new(r"[+\-*/]").unwrap();
+}
 
 fn main() {
     let mut quit: bool = false;
     let mut called_once: bool = false;
 
-    let re_number = Regex::new(r"\d+(\.\d+)?").unwrap();
-    let re_operator = Regex::new(r"[+\-*/]").unwrap();
     while !quit {
         if !called_once {
             println!("You can type q to quit the program");
@@ -25,30 +29,11 @@ fn main() {
             println!("The Programm will now exit");
             quit = true;
         } else {
-            if input_is_valid(&input, &re_number, &re_operator) {
+            if input_is_valid(&input) {
                 println!("input is valid");
 
-                let tokens = tokenize(&input.trim());
-                let mut result = 0.0;
-                let mut current_operator = '+';
+                let result: f64 = calculate_result(&input);
 
-                for token in tokens {
-                    if re_number.is_match(&token) {
-                        let number: f64 = token.parse().unwrap();
-
-                        match current_operator {
-                            '+' => result += number,
-                            '-' => result -= number,
-                            '*' => result *= number,
-                            '/' => result /= number,
-                            _ => panic!("Unknown operator: {}", current_operator),
-                        }
-                    } else if re_operator.is_match(&token) {
-                        current_operator = token.chars().next().unwrap();
-                    } else {
-                        panic!("Unknown token: {}", token);
-                    }
-                }
                 println!("Result: {}", result);
             } else {
                 println!("input is not valid")
@@ -57,11 +42,34 @@ fn main() {
     }
 }
 
-fn input_is_valid(input: &String, re_number: &Regex, re_operator: &Regex) -> bool {
-    //TODO
+fn calculate_result(input: &String) -> f64 {
+    let mut current_operator: char = '+';
+    let tokens: Vec<String> = tokenize(&input.trim());
+    let mut result: f64 = 0.0;
+    for token in tokens {
+        if RE_NUMBER.is_match(&token) {
+            let number: f64 = token.parse().unwrap();
+
+            match current_operator {
+                '+' => result += number,
+                '-' => result -= number,
+                '*' => result *= number,
+                '/' => result /= number,
+                _ => panic!("Unknown operator: {}", current_operator),
+            }
+        } else if RE_OPERATOR.is_match(&token) {
+            current_operator = token.chars().next().unwrap();
+        } else {
+            panic!("Unknown token: {}", token);
+        }
+    }
+    return result;
+}
+
+fn input_is_valid(input: &String) -> bool {
     let tokens = tokenize(&input.trim());
     for token in tokens {
-        if !re_number.is_match(&token) && !re_operator.is_match(&token) {
+        if !RE_NUMBER.is_match(&token) && !RE_OPERATOR.is_match(&token) {
             return false;
         }
     }
@@ -90,4 +98,29 @@ fn tokenize(input: &str) -> Vec<String> {
     }
 
     return tokens;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::calculate_result;
+    #[test]
+    fn basic() {
+        let input: String = String::from("1 +1-4     +54444");
+
+        let result = calculate_result(&input);
+        assert!(result == 54442.0);
+    }
+    #[test]
+    #[ignore = "unimplemented"]
+    fn point_before_line_calculation() {
+        let input: String = String::from("2+1*3");
+
+        let result: f64 = calculate_result(&input);
+        assert!(result == 5.0);
+
+        let input: String = String::from("2+1/4");
+
+        let result: f64 = calculate_result(&input);
+        assert!(result == 2.25);
+    }
 }
